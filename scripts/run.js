3a0afbafe51bd1d16e17c7034f03ea47682d3b76
= <=>
2021-12-28 14:20:16 -0700

    tested run.js, added test for other users

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,26 @@
 const main = async () => {
+
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+
+    console.log("Contract deployed by:", owner.address);
     console.log("Contract Deployed To:", waveContract.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
 }
 
 const runMain = async () => {
