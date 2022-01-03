cf81fb8a57919c59c4ced7f88522569c4ebcb0ef
Ro5hi <slen.detablan@gmail.com>
2022-01-03 16:13:40 -0700

    updated run script to share contract balance

--- scripts/run.js
+++ scripts/run.js
@@ -1,15 +1,31 @@
 const main = async () => {
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+      value: hre.ethers.utils.parseEther("0.1"),
+    });
     await waveContract.deployed();
     console.log("Contract address:", waveContract.address);
-  
+    
+    let contractBalance = await hre.ethers.provider.getBalance(
+      waveContract.address
+    );
+      console.log(
+        "Contract Balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+      );
+
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
     console.log(waveCount.toNumber());
 
     let waveTxn = await waveContract.wave("A message!");
     await waveTxn.wait(); 
+
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+      "Contract Balance:",
+      hre.ethers.utils.formatEther(contractBalance) 
+    );
   
     const [_, randomPerson] = await hre.ethers.getSigners();
     waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
