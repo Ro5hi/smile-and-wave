0dc4dc4f073ccaa3d1f9a32d7b64402607e032e3
Ro5hi <slen.detablan@gmail.com>
2022-01-03 16:40:41 -0700

    added 15min cooldown to wave

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,8 +6,6 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-
-    // number generator
     uint256 private seed;
 
    event NewWave(address indexed from, uint256 timestamp, string message);
@@ -18,8 +16,9 @@ contract WavePortal {
        uint256 timestamp;
    }
 
-
    Wave[] waves;
+
+   mapping(address => uint256) public lastWavedAt;
 
     constructor() payable {
         console.log("We have been constructed!");
@@ -28,13 +27,20 @@ contract WavePortal {
     }
 
     function wave(string memory _message) public {
+
+       require(
+           lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+           "Wait 15m"
+       );
+
+       lastWavedAt[msg.sender] = block.timestamp;
+
        totalWaves += 1;
        console.log("%s waved w/ message %s", msg.sender, _message);
 
        waves.push(Wave(msg.sender, _message, block.timestamp));
 
        seed = (block.difficulty + block.timestamp + seed) % 100;
-       console.log("Random # generated: %d", seed);
 
        if (seed <= 50) {
            console.log("%s won!", msg.sender);
