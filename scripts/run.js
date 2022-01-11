3c9b9f88bf8e0c31b2324ec399328940de856740
Ro5hi <slen.detablan@gmail.com>
2022-01-11 12:10:29 -0700

    changed value of eth

--- scripts/run.js
+++ scripts/run.js
@@ -1,41 +1,41 @@
 const main = async () => {
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy({
-      value: hre.ethers.utils.parseEther("0.1"),
-    });
-    await waveContract.deployed();
-    console.log("Contract address:", waveContract.address);
-    
-    let contractBalance = await hre.ethers.provider.getBalance(
-      waveContract.address
-    );
-      console.log(
-        "Contract Balance:",
-        hre.ethers.utils.formatEther(contractBalance)
-      );
-    
-    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await waveContract.deployed();
+  console.log("Contract address:", waveContract.address);
+  
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
     console.log(
       "Contract Balance:",
-      hre.ethers.utils.formatEther(contractBalance) 
+      hre.ethers.utils.formatEther(contractBalance)
     );
-   
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-    console.log(waveCount.toNumber());
+  
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Contract Balance:",
+    hre.ethers.utils.formatEther(contractBalance) 
+  );
+ 
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-    let allWaves = await waveContract.getAllWaves();
-    console.log(allWaves);
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
